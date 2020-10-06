b15dfbacccbeff06162c4528fb2da95082428b05
Joy Harjanto <38966343+joygraciacia@users.noreply.github.com>
2020-10-05 23:25:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, redirect, url_for, render_template
-import requests, json
 from flask_sqlalchemy import SQLAlchemy
 from opencage.geocoder import OpenCageGeocode
 from pprint import pprint
